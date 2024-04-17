728ffbe0fabdc89bb020a6005737050f69e3a57c
aturpin <aturpin@0131229EA121193.ad.cpp.edu>
2024-04-16 23:37:00 -0700

    Added 3-axis capability to the closed loop for motive and hexapod

--- NatNetSDK/Matlab/hexapod_motive_loop.m
+++ NatNetSDK/Matlab/hexapod_motive_loop.m
@@ -1,4 +1,11 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% S-Kubed 2024
+% Written by Andre Turpin & Matt Portugal
 clc; close all; clearvars -except C887 Controller devicesTcpIp ip matlabDriverPath port stageType use_TCPIP_Connection
+%
+%
+% Note: Hexapod moves an average of 0.0977mm per step
+
 
 %%%%%%%%%%%%%%%%%%% Hexapod Initialization %%%%%%%%%%%%%%%%%%%
 
@@ -64,61 +71,223 @@ end
 
 %%%%%%%%%%%%%%%% Prompt User Input for Desired Position %%%%%%%%%%%%%%%%
 
-prompt = "\n > Enter desired axis to move (X/Y/Z/U/V/W) \n \n >> ";
-input_axis = input(prompt,"s");
-if isempty(input_axis)
-    input_axis = 'Y'; 
+xDist = 0;
+yDist = 0;
+zDist = 0;
+
+fprintf('\nMax X: +/- 49 mm\n\n');
+xDist = input('Enter Movement in X-Direction: ');
+
+if xDist > 39 || xDist < -39
+    movementArray = [xDist, 0, 0];
+elseif xDist <= 39 && xDist > 16 || xDist < -16 && xDist >= -39
+    fprintf('\nMax Y: +/- 39 mm\n');
+    yDist = input('Enter Movement in Y-Direction: ');
+    movementArray = [xDist, yDist, 0];
+elseif xDist <= 16 || xDist <= -16
+    fprintf('\nMax Y: +/- 16 mm\n');
+    yDist = input('Enter Movement in Y-Direction: ');
+    fprintf('\nMax Z: +/- 16 mm\n');
+    zDist = input('Enter Movement in Z-Direction: ');
+    movementArray = [xDist, yDist, zDist];
 end
 
-if input_axis == 'X' || input_axis == 'Y' || input_axis == 'Z' || input_axis == 'U' || input_axis == 'V' || input_axis == 'W'
-    fprintf('\n Commanded axis to move is: %s\n \n',input_axis);
+% prompt = "\n > Enter desired axis to move (X/Y/Z/U/V/W) \n \n >> ";
+% input_axis = input(prompt,"s");
+% if isempty(input_axis)
+%     input_axis = 'Y';
+% end
+%
+% if input_axis == 'X' || input_axis == 'Y' || input_axis == 'Z' || input_axis == 'U' || input_axis == 'V' || input_axis == 'W'
+%     fprintf('\n Commanded axis to move is: %s\n \n',input_axis);
+%
+% else
+%     disp('ERROR: Axis not recognized, please enter from X,Y,Z,U,V,W')
+% end
+%
+% prompt2 = "\n > Enter desired movement (in mm) \n \n >> ";
+% input_Pos = input(prompt2,"s");
+% fprintf('\n Commanded desired position: %s mm \n',input_Pos);
 
-else
-    disp('ERROR: Axis not recognized, please enter from X,Y,Z,U,V,W')
-end
 
-prompt2 = "\n > Enter desired movement (in mm) \n \n >> ";
-input_Pos = input(prompt2,"s");
-fprintf('\n Commanded desired position: %s mm \n',input_Pos);
+%%%%%%%%%%%%%%%% Establish Initial Positions as read by Motive %%%%%%%%%%%%%%%%
 
+% Initial_Position = OptiTrak_Data(obj,input_axis);
+% Initial_Position = Initial_Position / 10; % Convert to mm
+% Initial_Position = abs(Initial_Position)
 
-%%%%%%%%%%%%%%%% Establish Initial Position as read by Motive %%%%%%%%%%%%%%%%
-
-Initial_Position = OptiTrak_Data(obj,input_axis);
-Initial_Position = Initial_Position / 10; % Convert to mm
-Initial_Position = abs(Initial_Position)
+InitialX = (OptiTrak_Data(obj,'X'))/10;
+InitialY = (OptiTrak_Data(obj,'Y'))/10;
+InitialZ = (OptiTrak_Data(obj,'Z'))/10;
 
 %%%%%%%%%%%%%%%% Start Condition Loop %%%%%%%%%%%%%%%%
 
-% Initial Conditions 
-DesiredPos = str2num(input_Pos); %0.5cm
-DesiredPos = DesiredPos;
-DesiredPos = DesiredPos - Initial_Position
-CurrentPos = Initial_Position;
-Error = DesiredPos - CurrentPos;
+% Initial Conditions
+% DesiredPos = str2num(input_Pos); %0.5cm
+% DesiredPos = DesiredPos - Initial_Position
+% CurrentPos = Initial_Position;
+% Error = DesiredPos - CurrentPos;
 
+% Define the desired positions
+DesiredX = xDist;
+DesiredY = yDist;
+DesiredZ = zDist;
 
-fprintf('The initial error prior to movement is %d \n \n',Error)
+% Adjust desired positions for any initial offset
+displacementX = DesiredX - InitialX;
+displacementY = DesiredY - InitialY;
+displacementZ = DesiredZ - InitialZ;
+
+% Update current position to the initial position
+CurrentX = abs(InitialX);
+CurrentY = abs(InitialY);
+CurrentZ = abs(InitialZ);
+
+% Compute Error at initial position
+xError = DesiredX - CurrentX;
+yError = DesiredY - CurrentY;
+zError = DesiredZ - CurrentZ;
+
+fprintf('The initial error prior to movement is: \n')
+fprintf('X-Error: [%0.3f]  Y-Error: [%0.3f]  Z-Error: [%0.3f]',xError,yError,zError)
+
+% Initialize Arrays for Plots
+xPositionArray = zeros();
+yPositionArray = zeros();
+zPositionArray = zeros();
+
+xStepArray = zeros();
+yStepArray = zeros();
+zStepArray = zeros();
+
+xErrorArray = zeros();
+yErrorArray = zeros();
+zErrorArray = zeros();
+
+xHexaPosArray = zeros();
+yHexaPosArray = zeros();
+zHexaPosArray = zeros();
+
+% Reset iterator
 n = 1;
+while xError >= 0.25
+    xError = DesiredX - CurrentX;
+    CurrentX = OptiTrak_Data(obj,'X');
+    CurrentX = abs(CurrentX);
 
-while Error >= 0.2
-
-  Error = DesiredPos - CurrentPos; %Recheck error
-    CurrentPos = OptiTrak_Data(obj,input_axis);
-    CurrentPos = abs(CurrentPos);
+    HexaPosX = C887.qPOS('X');
+    fprintf('--------------------------------------------- \n')
+    fprintf('Step | X-Error | CurrentPos | DesiredPos | HexapodPos \n')
+    fprintf('  %d | %0.3f |  %0.3f  |  %0.3f  | %0.3f \n',n,xError,CurrentX,DesiredX, HexaPosX)
   
-HexaPos = C887.qPOS('X');
-fprintf('--------------------------------------------- \n')
-fprintf('Step | Error | CurrentPos | DesiredPos | HexapodPos \n')
-fprintf('  %d | %0.3f |  %0.3f  |  %0.3f  | %0.3f \n',n,Error,CurrentPos,DesiredPos, HexaPos)
-% dataArray{n + 1} = {n, Error, CurrentPos, DesiredPos};
-
-C887.MOV(input_axis,0.1 * n) 
-% disp(C887.qPOS('X'))
-n = n + 1;
+    % Save Values to Arrays
+    xPositionArray = [xPositionArray, CurrentX];
+    xStepArray = [xStepArray,n];
+    xErrorArray = [xErrorArray,xError];
+    xHexaPosArray = [xHexaPosArray, HexaPosX];
+    
+    C887.MOV('X',0.1 * n)
+    % disp(C887.qPOS('X'))
+    n = n + 1;
 
 end
 
+pause(2)
+fprintf('\n \n  X-Direction has been corrected, continuing to the Y-Direction... \n \n \n')
+
+% Reset iterator
+n = 1;
+
+while yError >= 0.25
+    yError = DesiredY - CurrentY;
+    CurrentY = OptiTrak_Data(obj,'Y');
+    CurrentY = abs(CurrentY);
+
+    HexaPosY = C887.qPOS('Y');
+    fprintf('--------------------------------------------- \n')
+    fprintf('Step | Y-Error | CurrentPos | DesiredPos | HexapodPos \n')
+    fprintf('  %d | %0.3f |  %0.3f  |  %0.3f  | %0.3f \n',n,yError,CurrentY,DesiredY, HexaPosY)
+    
+    % Save Values to Arrays
+    yPositionArray = [yPositionArray, CurrentY];
+    yStepArray = [yStepArray,n];
+    yErrorArray = [yErrorArray,yError];
+    yHexaPosArray = [yHexaPosArray, HexaPosY];
+
+    C887.MOV('Y',0.1 * n)
+    % disp(C887.qPOS('X'))
+    n = n + 1;
+
+end
+
+pause(2)
+fprintf('\n \n  Y-Direction has been corrected, continuing to the Z-Direction... \n \n \n')
+
+% Reset iterator
+n = 1;
+
+while zError >= 0.25
+    zError = DesiredZ - CurrentZ;
+    CurrentZ = OptiTrak_Data(obj,'Z');
+    CurrentZ = abs(CurrentZ);
+
+    HexaPosZ = C887.qPOS('Z');
+    fprintf('--------------------------------------------- \n')
+    fprintf('Step | Z-Error | CurrentPos | DesiredPos | HexapodPos \n')
+    fprintf('  %d | %0.3f |  %0.3f  |  %0.3f  | %0.3f \n',n,zError,CurrentZ,DesiredZ, HexaPosZ)
+    
+    % Save Values to Arrays
+    zPositionArray = [zPositionArray, CurrentZ];
+    zStepArray = [zStepArray,n];
+    zErrorArray = [zErrorArray,zError];
+    zHexaPosArray = [zHexaPosArray, HexaPosZ];
+
+    C887.MOV('Z',0.1 * n)
+    % disp(C887.qPOS('X'))
+    n = n + 1;
+
+end
+
+% Plot Results
+figure(1)
+
+% X-Value Plots
+subplot(3,2,1)
+plot(xStepArray, xPositionArray,'LineWidth',1,'Color','b')
+hold on;
+plot(xStepArray, xHexaPosArray, 'LineWidth',1,'Color','r')
+legend('OptiTrack Position','Hexapod True Position')
+title('OptiTrack vs Hexapod Position [x]')
+
+subplot(3,2,2)
+plot(xStepArray,xErrorArray,'LineWidth',1,'Color',"#7E2F8E")
+title('Error for X')
+
+% Y-Value Plots
+subplot(3,2,3) 
+plot(yStepArray, yPositionArray, 'LineWidth', 1, 'Color', 'b')
+hold on;
+plot(yStepArray, yHexaPosArray, 'LineWidth', 1, 'Color', 'r')
+legend('OptiTrack Position', 'Hexapod True Position')
+title('OptiTrack vs Hexapod Position [y]')
+
+subplot(3,2,4)
+plot(yStepArray,yErrorArray,'LineWidth',1,'Color',"#7E2F8E")
+title('Error for Y')
+
+% Z-Value Plots
+subplot(3,2,5) 
+plot(zStepArray, zPositionArray,'LineWidth',1,'Color','b')
+hold on;
+plot(zStepArray, zHexaPosArray, 'LineWidth',1,'Color','r')
+legend('OptiTrack Position','Hexapod True Position')
+title('OptiTrack vs Hexapod Position [z]')
+
+subplot(3,2,6)
+plot(zStepArray,zErrorArray,'LineWidth',1,'Color',"#7E2F8E")
+title('Error for Z')
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% FUNCTIONS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function [rigidBody_Pos] = OptiTrak_Data(obj,input_axis) %Pulls current position of indicated axis from Motive
 
